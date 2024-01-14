3e5deac4af3870ca8ec61e865265f50541049e53
Sabid Hasan <sabid@sabids-air.home>
2024-01-14 20:46:23 +0000

    food landing done

--- js/swiper_foods.js
+++ js/swiper_foods.js
@@ -12,16 +12,12 @@ var swiper = new Swiper(".foods-first-slide", {
       },
 });
 
-var swiper = new Swiper(".categoriesSlider", {
+var swiper = new Swiper(".category-slider", {
     slidesPerView: 3,
     spaceBetween: 12,
     mousewheel: true,
     // grabCursor: true,
     centeredSlides: false,
-    // pagination: {
-    //   el: ".swiper-pagination",
-    //   clickable: true,
-    // },
     breakpoints: {
       640: {
         slidesPerView: 4,
@@ -38,7 +34,7 @@ var swiper = new Swiper(".categoriesSlider", {
     },
 });
 
-var swiper = new Swiper(".trendingFoods", {
+var swiper = new Swiper(".trending-foods", {
     slidesPerView: 1,
     spaceBetween: 12,
     // mousewheel: true,
@@ -63,15 +59,11 @@ var swiper = new Swiper(".trendingFoods", {
     },
 });
 
-var swiper = new Swiper(".featuredRestaurant", {
+var swiper = new Swiper(".featured-restaurant", {
     slidesPerView: 1,
     spaceBetween: 12,
     // mousewheel: true,
     // grabCursor: true,
-    // pagination: {
-    //   el: ".swiper-pagination",
-    //   clickable: true,
-    // },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
@@ -92,3 +84,49 @@ var swiper = new Swiper(".featuredRestaurant", {
     },
 });
 
+var swiper = new Swiper(".all-Category-foods", {
+    slidesPerView: 1,
+    spaceBetween: 12,
+    // mousewheel: true,
+    // grabCursor: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 16,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+    },
+});
+
+var swiper = new Swiper(".scheduled-food", {
+    slidesPerView: 1,
+    spaceBetween: 12,
+    mousewheel: true,
+    // grabCursor: true,
+    
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 12,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 14,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+    },
+});
